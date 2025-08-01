be132ea5151be8f264a92016edab44afa41f094a
aksdhanju <aksdhanju@gmail.com>
2025-08-01 22:34:18 +0530

    Initial commit for Calendar Booking System

---
+++ src/main/java/com/company/calendar/calendar_booking_system/CalendarBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.calendar.calendar_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/calendar/calendar_booking_system/CalendarBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.calendar.calendar_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
